a9b58c48424d72628c7309737d2e1f1123a78fd6
Alaa Awad <alaa.awad.mail@gmail.com>
2017-02-26 10:13:15 -0500

    instanceizing smiley faces

--- smiley_face/smiley.js
+++ smiley_face/smiley.js
@@ -4,67 +4,92 @@ var context = canvas.getContext('2d')
 context.lineWidth = 3;
 
 var posY = 150
-var eye_radius = width/12
-var width = canvas.width = window.innerWidth
-var height = canvas.height = window.innerHeight
+var smilers = [];
 
+var Smiley = function(offset) {
+  this.lineWidth = 3;
+  this.mouthLineWidth = 10;
+  this.width = canvas.width = window.innerWidth;
+  this.height = canvas.height = window.innerHeight;
+
+  this.eye_radius = this.width/12;
+
+  return this;
+};
+
+Smiley.prototype.render = function () {
+  context.clearRect(0, 0, this.width, this.height);
+  context.lineWidth = this.lineWidth;
+
+    // LEFT EYE
+  context.beginPath();
+  context.arc(this.width * 0.25, this.height * 0.25, this.width/12, 0, 2 * Math.PI, false);
+  context.stroke();
+
+    // LEFT EYE IRIS
+  context.beginPath();
+  context.arc(this.width * 0.25, this.height * 0.25, this.eye_radius, 0, 2 * Math.PI, false);
+  context.fill()
+  context.stroke();
+
+  // RIGHT EYE
+  context.beginPath();
+  context.arc(this.width * 0.75, this.height * 0.25, this.width/12, 0, 2 * Math.PI, false);
+  context.stroke();
+
+  // RIGHT EYE IRIS
+  context.beginPath();
+  context.arc(this.width * 0.75, this.height * 0.25, this.eye_radius, 0, 2 * Math.PI, false);
+  context.fill()
+  context.stroke();
+
+
+  // MOUTH
+  context.lineWidth = this.mouthLineWidth;
+  context.beginPath()
+  context.moveTo(this.width * 0.25, this.height * 0.75) // starting point. left edge of mouth
+  context.quadraticCurveTo(this.width * 0.5, posY, this.width * 0.75, this.height * 0.75)
+  context.stroke()
+
+  return this;
+};
 
 // MATHY THINGS
 var scale = function(num,mapMin, mapMax) {
     let baseMin = 0
-    let baseMax = height
+    let baseMax = canvas.height
 
     return ((mapMax - mapMin) * (num - baseMin) / (baseMax - baseMin)) + mapMin;
 }
 
 // EVENTS
 var onMouseMove = function(event) {
-    eye_radius = scale(event.pageY, width/24, width/12)
-    posY = scale(event.pageY, height/2, height);
+  if (!smilers) return;
+  smilers.forEach(function(smiler, index) {
+    smiler.eye_radius = scale(event.pageY, smiler.width/24, smiler.width/12)
+    posY = scale(event.pageY, smiler.height/2, smiler.height);  
+  });
 }
 window.addEventListener("mousemove", onMouseMove);
 
 var resize = function() {
-  width = canvas.width = window.innerWidth
-  height = canvas.height = window.innerHeight
+  smilers.forEach(function(smiler, index) {
+    smielr.width = canvas.width = window.innerWidth
+    smiler.height = canvas.height = window.innerHeight
+  });
 }
 window.addEventListener("resize", resize);
 
-// RENDER EVERY PAGE REFRESH
-var render = function() {
-  context.clearRect(0, 0, width, height);
-  context.lineWidth = 3;
+smilers = [
+  new Smiley(),
+]
 
-    // LEFT EYE
-  context.beginPath();
-  context.arc(width * 0.25, height * 0.25, width/12, 0, 2 * Math.PI, false);
-  context.stroke();
+var renderSmiley = function() {
+  smilers.forEach(function(smiler, index) {
+    smiler.render();
+  });
 
-    // LEFT EYE IRIS
-  context.beginPath();
-  context.arc(width * 0.25, height * 0.25, eye_radius, 0, 2 * Math.PI, false);
-  context.fill()
-  context.stroke();
-
-  // RIGHT EYE
-  context.beginPath();
-  context.arc(width * 0.75, height * 0.25, width/12, 0, 2 * Math.PI, false);
-  context.stroke();
-
-  // RIGHT EYE IRIS
-  context.beginPath();
-  context.arc(width * 0.75, height * 0.25, eye_radius, 0, 2 * Math.PI, false);
-  context.fill()
-  context.stroke();
-
-
-  // MOUTH
-  context.lineWidth = 10;
-  context.beginPath()
-  context.moveTo(width * 0.25, height * 0.75) // starting point. left edge of mouth
-  context.quadraticCurveTo(width * 0.5, posY, width * 0.75, height * 0.75)
-  context.stroke()
-
-  window.requestAnimationFrame(render)
+  window.requestAnimationFrame(renderSmiley)
 }
-render()
+
+renderSmiley()
